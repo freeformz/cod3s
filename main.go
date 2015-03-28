5214f890ccaa8b37ed6c0233034acfe5a54b34d4
Edward Muller <edwardam@interlix.com>
2015-03-27 18:08:51 -0700

    Log some

--- main.go
+++ main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
 )
 
+// Extract a HTTP code from the end of a path.
+// e.x. /foo/bar/200 == 200
+// If the last part of the path can't convert to an int, then return
+// http.StatusBadRequest
+func codeFromPath(p string) int {
+	pathParts := strings.Split(p, "/")
+	statusCode, err := strconv.Atoi(pathParts[len(pathParts)-1])
+	if err != nil {
+		statusCode = http.StatusBadRequest
+	}
+	return statusCode
+}
+
 func main() {
+	log.SetFlags(0)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		pathParts := strings.Split(r.URL.Path, "/")
-		statusCode, err := strconv.Atoi(pathParts[len(pathParts)-1])
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
+		statusCode := codeFromPath(r.URL.Path)
+
+		log.Printf("path=%q host=%q remote=%q status=%d\n", r.URL.Path, r.Host, r.RemoteAddr, statusCode)
 
 		w.WriteHeader(statusCode)
 	})
